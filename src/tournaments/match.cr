4bbefdb28c91e0207049f007b268746f38f668ab
badBlackShark <Timdaniel.Engelhardt@web.de>
2018-10-03 22:42:32 +0200

    Basic Match class.

--- src/tournaments/match.cr
+++ src/tournaments/match.cr
@@ -1,7 +1,14 @@
 class Match
   include YAML::Serializable
 
-  def initialize(*args)
+  getter participants : Array(UInt64)
+  getter time : Time
+  getter id : Int32
 
+  def initialize(@participants : Array(UInt64), @time : Time, @id : Int32)
+  end
+
+  def to_s
+    "Match ##{@id}: #{participants.map { |e| "<@#{e}>" }.join(" vs ")} on #{FORMATTER.parse(@time)}"
   end
 end
