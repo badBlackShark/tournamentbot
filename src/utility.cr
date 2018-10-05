a31753a3a66e60df04eb76a7d6af518caf74fdd6
badBlackShark <Timdaniel.Engelhardt@web.de>
2018-10-05 21:16:42 +0200

    Made the module stateless.

--- src/utility.cr
+++ src/utility.cr
@@ -2,23 +2,16 @@ require "time"
 require "tasker"
 
 module TournamentBot::Utility
-  @@formatter = Time::Format.new("%A, %-d.%-m.%Y at %I:%M%p UTC", Time::Location.fixed("UTC", 0))
-  @@scheduler : Tasker = Tasker.instance
-
-  def self.store_client(client : Discord::Client)
-    @@client = client
-  end
-
   def self.format_time(time : Time) : String
-    @@formatter.format(time)
+    Time::Format.new("%A, %-d.%-m.%Y at %I:%M%p UTC", Time::Location.fixed("UTC", 0)).format(time)
   end
 
   def self.schedule_reminder(targets : Array(UInt64), message : String, time : Time)
-    @@scheduler.at(time) do
+    Tasker.instance.at(time) do
       targets.each do |target|
-        channel = @@client.try &.cache.try &.resolve_dm_channel(target)
+        channel = TournamentBot.bot().cache.resolve_dm_channel(target)
         if channel
-          @@client.try &.create_message(channel, message)
+          TournamentBot.bot().client.create_message(channel, message)
         end
       end
     end
