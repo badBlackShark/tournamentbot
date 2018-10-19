bf7e011e7c53b9baf9f7dda78e5fa2150236304e
Zac Nowicki <zachnowicki@gmail.com>
2018-10-19 16:02:09 +0000

    Fix missing method call in middleware

--- src/middlewares/ban_checker.cr
+++ src/middlewares/ban_checker.cr
@@ -3,11 +3,11 @@ class TournamentBot::BanChecker
   end
 
   def call(payload : Discord::Message, context)
-    bans_left = @drafts[payload.channel_id]
+    bans_left = @drafts[payload.channel_id].bans_left
     return yield if bans_left.zero? && @ban_stage_complete
 
     if bans_left.zero? && !@ban_stage_complete
-      context[Discord::Client].create_messsgae(payload.channel_id, "The ban stage has already finished, so you can't use this command anymore.")
+      context[Discord::Client].create_message(payload.channel_id, "The ban stage has already finished, so you can't use this command anymore.")
     end
 
     if !bans_left.zero? && @ban_stage_complete
